51313974e3e14e046af8cbef479b6e470b0abb1f
unknown <laurent.quastana@gmail.com>
2019-10-26 11:04:32 +0200

    module 3

--- jobs/app.py
+++ jobs/app.py
@@ -9,6 +9,8 @@ def open_connection():
     connection = getattr(g,'_connection',None)
     if connection is None:
         connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
 
 def execute_sql(sql, values=(), commit=False,single=False):
     connection = open_connection()
